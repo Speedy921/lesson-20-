a94130f38f9aafbd6a6c54ee5c8cbb0e0246f635
Artem Viatkin <newyorkbox47@gmail.com>
2023-01-13 16:29:59 +0200

    Добавьте файлы проекта.

---
+++ lesson 20/Program.cs
@@ -0,0 +1,12 @@
+﻿using lesson_20;
+
+Console.WriteLine("****** Simple Exception Example****");
+Console.WriteLine("=> Creating a car and stepping on it!");
+Car myCar = new Car("Zippy", 20);
+myCar.CrankTunes(true);
+
+for (int i = 0; i < 10; i++)
+{
+    myCar.Accelerate(10);
+}
+Console.ReadLine();
